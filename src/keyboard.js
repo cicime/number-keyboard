560ce9681cc7cc3ae31bbe6d4e0baf854b4b4c43
cicime <yuanming200755@163.com>
2017-03-02 15:00:50 +0800

    add code ❤ mua

--- src/keyboard.js
+++ src/keyboard.js
@@ -1,6 +1,30 @@
 'use strict'
-
 export default function (config) {
   const cfg = Object.assign({}, config)
-  console.log(89)
+
+  const key = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [0, '.', 'back']
+  ]
+
+  const html = document.createElement('div'),
+        table = document.createElement('table')
+  html.className = 'keyboard'
+  table.className = 'keyboard-table'
+
+  for (let i = 0; i < key.length; i++) {
+    let row = document.createElement('tr')
+    for(let j = 0; j < key[i].length; j++){
+      let td = document.createElement('td')
+      td.innerHTML = key[i][j]
+      td.className = 'keyboard-key'
+      row.appendChild(td)
+    }
+    table.appendChild(row)
+  }
+
+  html.appendChild(table)
+  document.body.appendChild(html)
 }
